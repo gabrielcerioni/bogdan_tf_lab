37a5038e03c698e69014611e02080928321301eb
gabrielcerioni <80788917+gabrielcerioni@users.noreply.github.com>
2021-05-25 10:53:07 -0300

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,18 +1,15 @@
 terraform {
-  required_version = "0.14.9"
-
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 3.61.0"
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
     }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = ">= 3.61.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.3"
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.0.0"
     }
   }
+
+  required_version = "~> 0.14"
 }
